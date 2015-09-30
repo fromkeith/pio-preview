1e05e524ca3993ae5e5c7c6b47b6b68f6712466b
fromkeith <keith@fromkeith.com>
2015-09-30 10:55:42 -0700

    switch to use '=' instead of '@' in directives. Don't load images until the ppSrc has a value -> Stopping us from hitting the cdn with invalid urls.

--- pio-preview.js
+++ pio-preview.js
@@ -19,49 +19,104 @@
     'use strict';
 
     return angular.module('pio-preview', [])
-        .directive('ppMousepad', function () {
+        .directive('ppMousepad', ['$compile', function ($compile) {
             return {
                 scope: {
-                    ppSrc: '@'
+                    ppSrc: '='
                 },
                 restrict: 'E',
-                replace: true,
-                template: '<div class="pp-mousepad"><div class="pp-img-wrapper"><div class="pp-img" style="background-image: url({{ppSrc}})"></div></div><img src="https://az412349.vo.msecnd.net/product-mousepads/mousepad-new-backv1.png" class="pp-bkg"><img src="https://az412349.vo.msecnd.net/product-mousepads/Mousepad-new-overlayv1.png" class="pp-border"></div>'
+                template: '<div class="pp-mousepad"><div class="pp-img-wrapper"><div class="pp-img" style="{{ppStyle}}"></div></div><img src="https://az412349.vo.msecnd.net/product-mousepads/mousepad-new-backv1.png" class="pp-bkg"><img src="https://az412349.vo.msecnd.net/product-mousepads/Mousepad-new-overlayv1.png" class="pp-border"></div>',
+                link: function (scope, element, attrs) {
+                    function updateStyle() {
+                        if (!scope.ppSrc) {
+                            return;
+                        }
+                        scope.ppStyle = 'background-image: url(' + scope.ppSrc + ')';
+                    }
+                    if (scope.ppSrc) {
+                        updateStyle();
+                    }
+                    scope.$watch('ppSrc', function () {
+                        updateStyle();
+                    });
+                }
             };
-        })
+        }])
         .directive('ppFramed', function () {
             return {
                 scope: {
-                    ppSrc: '@'
+                    ppSrc: '='
                 },
                 restrict: 'E',
-                replace: true,
-                template: '<div class="pp-framed"><div class="pp-img-wrapper"><div class="pp-img" style="background-image:url({{ppSrc}});"></div></div><img src="https://az412349.vo.msecnd.net/widget-assets/FFFFFF-0.png" class="pp-bkg"><img src="https://az412349.vo.msecnd.net/product-framedprints/12x18OverlayBlack.png" class="pp-border"></div>'
+                template: '<div class="pp-framed"><div class="pp-img-wrapper"><div class="pp-img" style="{{ppStyle}}"></div></div><img src="https://az412349.vo.msecnd.net/widget-assets/FFFFFF-0.png" class="pp-bkg"><img src="https://az412349.vo.msecnd.net/product-framedprints/12x18OverlayBlack.png" class="pp-border"></div>',
+                link: function (scope, element, attrs) {
+                    function updateStyle() {
+                        if (!scope.ppSrc) {
+                            return;
+                        }
+                        scope.ppStyle = 'background-image: url(' + scope.ppSrc + ')';
+                    }
+                    if (scope.ppSrc) {
+                        updateStyle();
+                    }
+                    scope.$watch('ppSrc', function () {
+                        updateStyle();
+                    });
+                }
             };
         }).directive('ppMug', function () {
             return {
                 scope: {
-                    ppSrc: '@'
+                    ppSrc: '='
                 },
                 restrict: 'E',
-                replace: true,
-                template: '<div class="pp-mug"><div class="pp-img-wrapper"><div class="pp-img" style="background-image:url({{ppSrc}})"></div></div><img src="https://az412349.vo.msecnd.net/product-mugs/Mug11oz-Backgroundv4.png" class="pp-bkg"><img src="https://az412349.vo.msecnd.net/product-mugs/Mug11oz-Overlayv3.png" class="pp-border"></div>'
+                template: '<div class="pp-mug"><div class="pp-img-wrapper"><div class="pp-img" style="{{ppStyle}}"></div></div><img src="https://az412349.vo.msecnd.net/product-mugs/Mug11oz-Backgroundv4.png" class="pp-bkg"><img src="https://az412349.vo.msecnd.net/product-mugs/Mug11oz-Overlayv3.png" class="pp-border"></div>',
+                link: function (scope, element, attrs) {
+                    function updateStyle() {
+                        if (!scope.ppSrc) {
+                            return;
+                        }
+                        scope.ppStyle = 'background-image: url(' + scope.ppSrc + ')';
+                    }
+                    if (scope.ppSrc) {
+                        updateStyle();
+                    }
+                    scope.$watch('ppSrc', function () {
+                        updateStyle();
+                    });
+                }
             };
         }).directive('ppPhone', function () {
             return {
                 scope: {
-                    ppSrc: '@',
+                    ppSrc: '=',
                     // valid types:
                     // [iPhone4S, iPhone5C, iPhone5, GalaxyS2, GalaxyS3, GalaxyS4, GalaxyNote, GalaxyNote2, GalaxyNote3, HTCOne]
-                    ppType: '@'
+                    ppType: '='
                 },
                 restrict: 'E',
-                replace: true,
-                template: '<div class="pp-phone"><div class="pp-img-wrapper"><div class="pp-img" style="background-image: url({{ppSrc}});"></div></div><img src="https://az412349.vo.msecnd.net/product-phonecases/{{ppType}}-back.png" class="pp-bkg"><img src="https://az412349.vo.msecnd.net/product-phonecases/{{ppType}}-overlay.png" class="pp-border"></div>',
-                link: function ($scope, element, attrs) {
-                    if ($scope.ppType === undefined || $scope.ppType === '') {
-                        $scope.ppType = 'iPhone4s';
+                template: '<div class="pp-phone"><div class="pp-img-wrapper"><div class="pp-img" style="{{ppStyle}};"></div></div><img ng-src="{{ppBack}}" class="pp-bkg"><img ng-src="{{ppOverlay}}"" class="pp-border"></div>',
+                link: function (scope, element, attrs) {
+                    function updateStyle() {
+                        if (!scope.ppSrc) {
+                            return;
+                        }
+                        if (scope.ppType === undefined || scope.ppType === '') {
+                            scope.ppType = 'iPhone4s';
+                        }
+                        scope.ppStyle = 'background-image: url(' + scope.ppSrc + ')';
+                        scope.ppBack = 'https://az412349.vo.msecnd.net/product-phonecases/' + scope.ppType + '-back.png';
+                        scope.ppOverlay = 'https://az412349.vo.msecnd.net/product-phonecases/' + scope.ppType + '-overlay.png';
                     }
+                    if (scope.ppSrc) {
+                        updateStyle();
+                    }
+                    scope.$watch('ppSrc', function () {
+                        updateStyle();
+                    });
+                    scope.$watch('ppType', function () {
+                        updateStyle();
+                    });
                 }
             };
         });
